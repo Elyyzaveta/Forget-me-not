3c33d9fc6ac740c3e2a663d7ed5341b3baa57423
Elyyzaveta <51882432+Elyyzaveta@users.noreply.github.com>
2019-10-25 21:40:24 +0300

    bagv3.1

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
+var i = 0;
 
+function createTask() {
+    var taskName = $("#myTask").val();
+    $("body").append("<div class='tsk'>" + taskName + "</div>");
 
-   function createTask() {
-       var taskName = $("#myTask").val();
-       $("body").append("<div id='containerWithTask'>" + taskName + "<button id='btnDel' type='button'/>" +"</div>");
-       var i=0;
-       $('div').each(function(){
-           i++;
-           var newID='#myTask'+i;
-           $(this).attr('id',newID);
-           $(this).val(i);
-       });
-   };
-   $("#btnDel").onclick(function () {
-       $("#myTask").remove();
-   });
+    $(".tsk").each(function () {
+        i++;
+        $(this).attr("id",);
+        $(this).val(i);
+    });
+};
+
