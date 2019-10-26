07fb2719c8c35f54540f20a440b53c663f2c8f1c
Volodymyr Ovcharenko <Volodymyyrr@gmail.com>
2019-10-26 23:12:36 +0300

    added id to created task

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ var i = 0;
 
 function createTask() {
     var taskName = $("#myTask").val();
-    $("body").append("<div class='tsk'>" + taskName + "</div>");
-
-    $(".tsk").each(function () {
-        i++;
-        $(this).attr("id",);
-        $(this).val(i);
-    });
+    $("body").append("<div class='tsk' id=" + i + " onclick='remove(this.id)'>" + taskName + "</div>");
+    i++;
 };
+
+function remove(id) {
+	console.log(id);	
+}
 
